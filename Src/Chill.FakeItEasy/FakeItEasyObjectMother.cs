bcee3866577ed0edd68f7115bef8ca6aa51fda6c
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2019-02-20 21:36:17 +0100

    SAVEPOINT

--- Src/Chill.FakeItEasy/FakeItEasyObjectMother.cs
+++ Src/Chill.FakeItEasy/FakeItEasyObjectMother.cs
@@ -1,30 +1,29 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
-using FakeItEasy;
 
 namespace Chill.FakeItEasy
 {
     /// <summary>
     /// An object mother that uses FakeItEasy to generate stubs.
     /// </summary>
-    
+
     // ReSharper disable once UnusedMember.Global
     public class FakeItEasyObjectMother : IObjectMother
     {
+        public object Create(Type type, IChillObjectResolver objectResolver)
+        {
+            return global::FakeItEasy.Sdk.Create.Fake(type);
+        }
+
         public bool IsFallback => true;
 
         public bool Applies(Type type)
         {
-            return 
-                type.GetTypeInfo().IsInterface && 
-                !(type.GetTypeInfo().IsGenericType &&  type.GetGenericTypeDefinition() != typeof(IEnumerable<>)) &&
+            return
+                type.GetTypeInfo().IsInterface &&
+                !(type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() != typeof(IEnumerable<>)) &&
                 !type.IsArray;
-        }
-
-        public T Create<T>(IChillContainer container) where T : class
-        {
-            return A.Fake<T>();
         }
     }
 }
